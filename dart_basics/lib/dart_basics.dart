72ab618b783b833cb9e4f698e64a85ae1fdd2f42
oleg.nazarenko.dev <oleg.nazarenko.dev@gmail.com>
2021-11-21 12:50:29 +0200

    Доделал ДЗ.

--- dart_basics/lib/dart_basics.dart
+++ dart_basics/lib/dart_basics.dart
@@ -119,3 +119,137 @@ class Converter {
   }
 
 }
+
+class Point {
+  int X;
+  int Y;
+  int Z;
+
+  Point(this.X, this.Y, this.Z);
+
+  factory Point.zero() {
+    return Point(0, 0, 0);
+  }
+
+  factory Point.one() {
+    return Point(1, 1, 1);
+  }
+
+  factory Point.oneX() {
+    return Point(1, 0, 0);
+  }
+
+  factory Point.oneY() {
+    return Point(0, 1, 0);
+  }
+
+  factory Point.oneZ() {
+    return Point(0, 0, 1);
+  }
+
+  double distanceTo(Point other) {
+    var x = other.X - X;
+    x *= x;
+
+    var y = other.Y - Y;
+    y *= y;
+
+    var z = other.Z - Z;
+    z *= z;
+
+    return sqrt(x + y + z);
+  }
+
+  static double getTriangleSquare(Point a, Point b, Point c) {
+    var ab = a.distanceTo(b);
+    var bc = b.distanceTo(c);
+    var ac = a.distanceTo(c);
+
+    var p = (ab + bc + ac) / 2;
+
+    return sqrt(p * (p - ab) * (p - bc) * (p - ac));
+  }
+}
+
+class MySqrt {
+  static double calc(double value, int n) {
+    if (value < 0 && (n & 1) == 0) {
+      throw ArgumentError('Корень чётной степени из отрицательного числа не существует в области вещественных чисел.');
+    }
+    
+    double eps = 0.00001;
+    double root = value / n;
+    double rn = value;
+    
+    int counter = 0;
+    
+    while(_abs(root - rn) >= eps){
+        rn = value;
+
+        for(int i = 1; i < n; i++) {
+            rn = rn / root;
+        }
+
+        root = 0.5 * (rn + root);
+        
+        ++counter;
+    }
+
+    return root;
+  }
+
+  static double _abs(double value) {
+    if (value < 0) {
+      return -value;
+    }
+
+    return value;
+  }
+}
+
+class User {
+  String email;
+
+  User(this.email);
+}
+
+class AdminUser extends User with MailSystem {
+  AdminUser(String email) : super(email);
+}
+
+class GeneralUser extends User {
+  GeneralUser(String email) : super(email);
+}
+
+mixin MailSystem implements User {
+  String getMailSystem() {
+    return email.split('@').last;
+  }
+}
+
+class UserManager<T extends User> {
+  final List<T> _users = <T>[];
+
+  void add(T user) {
+    _users.add(user);
+  }
+
+  bool remove(T user) {
+    return _users.remove(user);
+  }
+
+  List<String> getAllUserEmails() {
+    List<String> result = <String>[];
+
+    for (var user in _users) {
+      if (user is AdminUser) {
+        result.add(user.getMailSystem());
+      }
+      else {
+        result.add(user.email);
+      }
+    }
+
+    return result;
+  }
+}
--- dart_basics/test/dart_basics_test.dart
+++ dart_basics/test/dart_basics_test.dart
@@ -17,4 +17,22 @@ void main() {
   print(converter.toMap([ 'a', 'b', 'a', 'ab', 'a' ]));
   print(converter.fromNumbersNamesToNumbersList('one, two, zero, zero'));
   print(converter.fromNumbersNamesToNumbersList('one, two, three, cat, dog'));
+
+  Point a = Point.oneX();
+  Point b = Point(12, 23, -6);
+  Point c = Point.oneZ();
+
+  print('Площадь треугольника: ' + Point.getTriangleSquare(a, b, c).toString());
+
+  print('Квадратный корень третьей степени из 27-ми = ' + MySqrt.calc(27, 3).toString());
+
+  AdminUser admin = AdminUser('address@google.com');
+  print(admin.getMailSystem());
+
+  GeneralUser general = GeneralUser('test@ucoz.com');
+  UserManager manager = UserManager();
+  manager.add(admin);
+  manager.add(general);
+  
+  print(manager.getAllUserEmails());
 }
